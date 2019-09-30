ef1e7e0c6374655c993de6e43dced11fbe3a5472
Vijjasena <13517084@std.stei.itb.ac.id>
2019-09-30 19:23:06 +0700

    Add checkWin function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
+import os
 
-def printBoard (gameBoard) :
+def printBoard (gameBoard,enable) :
+    if(enable):
+        os.system('cls' if os.name == 'nt' else 'clear')
     for i in range(6):
         for j in range(7):
             print(gameBoard[i][j], end = '')
@@ -39,19 +42,55 @@ def checkFullBoard (gameBoard):
             return False
     return True
 
+def checkWin(gameBoard,piece):
+    #Check horizontal condition
+    for c in range(4):
+        for r in range(6):
+            if(gameBoard[r][c] == gameBoard[r][c+1] == gameBoard[r][c+2] == gameBoard[r][c+3] == piece):
+                return True
+            
+
+    #Check vertical condition
+    for c in range(7):
+        for r in range(3):
+            if(gameBoard[r][c] == gameBoard[r+1][c] == gameBoard[r+2][c] == gameBoard[r+3][c] == piece):
+                return True
+            
+
+    #Check positive slope
+    for c in range(4):
+        for r in range(3):
+            if(gameBoard[r][c] == gameBoard[r+1][c+1] == gameBoard[r+2][c+2] == gameBoard[r+3][c+3] == piece):
+                return True
+            
+
+    #Check negative slope
+    for c in range(4):
+        for r in range(3,6):
+            if(gameBoard[r][c] == gameBoard[r-1][c+1] == gameBoard[r-2][c+2] == gameBoard[r-3][c+3] == piece):
+                return True
+
+
+PLAYER = 1
+AI = 2
+clear_screen = False
 gameBoard = [[0 for col in range(7)] for row in range(6)]
 # Board structure
-#   1  2  3  4  5  6  7
+#   1  2  3  4  5  6  7 => column
 # 1 0  0  0  0  0  0  0
 # 2 0  0  0  0  0  0  0
 # 3 0  0  0  0  0  0  0
 # 4 0  0  0  0  0  0  0
 # 5 0  0  0  0  0  0  0
 # 6 0  0  0  0  0  0  0
-printBoard(gameBoard)
-while (not checkEndCondition(gameBoard)):
-    updateBoard(1, inputStep(gameBoard), gameBoard)
-    updateBoard(2, randomizeMove(gameBoard), gameBoard)
-    printBoard(gameBoard)
+printBoard(gameBoard,clear_screen)
+win = False
+
+while (not win):
+    updateBoard(PLAYER, inputStep(gameBoard), gameBoard)
+    updateBoard(AI, randomizeMove(gameBoard), gameBoard)
+    printBoard(gameBoard,clear_screen)
+    if(checkWin(gameBoard,PLAYER) or checkWin(gameBoard,AI)):
+        win = True
 
 print('Permainan berakhir!!!')
